d889519a8343751cfc21c8a43e13cda65180002f
faithbabanla <kolapofaith@gmail.com>
2023-05-03 18:30:18 -0400

    added in our addToCart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -13,7 +13,7 @@ const CartIcon = () => {
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item">0</span>
+      <span className="item">{cartItemCount}</span>
     </div>
   );
 };
